a5030c20a3d23a74e9613322c3ea6f4ccde93987
viswaamitcodes <71846247+viswaamitcodes@users.noreply.github.com>
2021-06-10 11:42:43 +0530

    Update 2_problem_r1.py

--- 2_problem_r1.py
+++ 2_problem_r1.py
@@ -6,22 +6,22 @@ def is_prime(n):
         if n%i==0:
             return False
         return True
-    for i in range(int(input())):
-        fi,la=map(int,input().split())
-        f=l=0
-        for j in range(fi,la+1):
-            if f==0 and is_prime(fi):
-                f=fi
-            else:
-                fi+=1
-            if l==0 and is_prime(la):
-                l=la
-            else:
-                la-=1
-        if f!=0 and l!=0:
-            print(l-f)
+for i in range(int(input())):
+    fi,la=map(int,input().split())
+    f=l=0
+    for j in range(fi,la+1):
+        if f==0 and is_prime(fi):
+            f=fi
         else:
-            print(-1)
+            fi+=1
+        if l==0 and is_prime(la):
+            l=la
+        else:
+            la-=1
+    if f!=0 and l!=0:
+        print(l-f)
+    else:
+        print(-1)
 
 
 
